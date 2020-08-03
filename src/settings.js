a325e1f9b894a8ad719e685770e2991221b9e510
panlinjun <376151097@qq.com>
2020-08-03 17:22:43 +0800

    '修改配置文件'

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
   name: 'PanLinjun',
   clientID: 'Iv1.adc77b1196779ab4',
-  clientSecret: 'a1c9cc81398b9f2e1a12c207dd2b368319610316'
+  clientSecret: '1fb392919fd47e2810c6dac60652714ae8c8cf86'
 }
